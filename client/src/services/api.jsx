b9062677bc78fd53adea64e7f0fdf7099f40173b
Rishu Raj <rishuraj21387@gmail.com>
2026-01-05 14:40:25 +0530

    Update frontend API URL for production

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://personal-portfolio-final-copy-1.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
